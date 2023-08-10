681a60b563295b66f6c3e75431a1780c17173d69
John Graham <110198683+jharrisong830@users.noreply.github.com>
2023-08-10 13:24:12 -0400

    can load songs from spotify

---
+++ SimpleMusic/Views/ImportPlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  ImportPlaylistView.swift
+//  SimpleMusic
+//
+//  Created by John Graham on 8/10/23.
+//
+
+import SwiftUI
+
+struct ImportPlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImportPlaylistView()
+}
---
+++ SimpleMusic/Views/SongRow.swift
@@ -0,0 +1,18 @@
+//
+//  SongRow.swift
+//  SimpleMusic
+//
+//  Created by John Graham on 8/9/23.
+//
+
+import SwiftUI
+
+struct SongRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongRow()
+}
